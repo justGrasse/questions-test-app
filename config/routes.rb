82429dbd88a4a4b74ae45d43580276f2a1dfcfd1
Justin G <email": null>
2017-01-23 20:27:20 -0800

    create routes without rsources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :questions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :questions
+
+  get 'questions/' => 'questions#index'
+  get 'questions/new' => 'questions#new',
+        as: 'new_question'
+  post 'questions' => 'questions#create'
+  get 'questions/:id' => 'questions#show',
+        as: 'question'
+  get 'questions/:id/edit' => 'questions#edit',
+        as: 'edit_question'
+  patch 'questions/:id' => 'questions#update'
+  delete 'questions/:id' => 'questions#destroy'
+
 end
